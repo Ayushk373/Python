242322c2e653c62b51df3c3bb4b3c376065926a5
Ayushk373 <ayushkhude373@gmail.com>
2026-08-10 19:36:18 +0530

    Add basic operations and list examples
    Implement basic operations and list manipulations in BasicOp.py

---
+++ BasicOp.py
@@ -0,0 +1,17 @@
+a=int(input("enter number 1 :"))
+b=int(input("enter number 2 :"))
+c=int(input("enter number 3 :"))
+print("using and operators:",a>b and a>c)
+print("using or operators:",a>b or a>c)
+print("using not operators:", not a>b and a>c)
+print("shree swami smarath")
+
+my_list = [10, 20, 30, "Ayush", 5.5]
+print(my_list[0])
+my_list.append(40)
+my_list.insert(1,90)
+my_list.remove(20)
+my_list.pop()
+print(my_list[0:4])  
+for item in [1, 2, 3]:
+    print(item)
