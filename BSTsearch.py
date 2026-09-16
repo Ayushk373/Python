4944ce381855d9f9261e48711348a8c9e7377444
Ayush Santosh Khude <ayushkhude373@gmail.com>
2026-09-16 14:47:52 +0530

    Update BSTsearch.py

--- BSTsearch.py
+++ BSTsearch.py
@@ -1,1 +1,12 @@
+def search(root, key):
 
+    if root is None:
+        return False
+
+    if root.data == key:
+        return True
+
+    if key < root.data:
+        return search(root.left, key)
+
+    return search(root.right, key)
