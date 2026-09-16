a300379b00c3664def5baea4ad46fea1dd8bba10
Ayush Santosh Khude <ayushkhude373@gmail.com>
2026-09-16 11:29:47 +0530

    Create QueneList.py

---
+++ QueneList.py
@@ -0,0 +1,13 @@
+queue = []
+
+# Enqueue
+queue.append(10)
+queue.append(20)
+queue.append(30)
+
+print("Queue:", queue)
+
+# Dequeue
+print("Removed:", queue.pop(0))
+
+print("Queue after dequeue:", queue)
