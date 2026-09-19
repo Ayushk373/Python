31fc4c0a4be88e94e7532d0eb489ffb83f6d4e29
Ayush Santosh Khude <ayushkhude373@gmail.com>
2026-09-19 23:48:30 +0530

    Add Evenodd2.py to check if a number is even or odd

---
+++ Evenodd2.py
@@ -0,0 +1,8 @@
+# Get a number from the user
+num = int(input("Enter a number to check: "))
+
+# Check if the number is perfectly divisible by 2
+if num % 2 == 0:
+    print("That number is Even!")
+else:
+    print("That number is Odd!")
